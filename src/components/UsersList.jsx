9e016da969ac2625f804e2d5f909654cd87bbd57
Mateo <miketry21@gmail.com>
2023-02-16 19:00:41 -0500

    Arreglos maquetado

--- src/components/UsersList.jsx
+++ src/components/UsersList.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import UserCard from "./UserCard"
+import "./styles/UserList.css"
 
 const UsersList = ({users, deleteUser, setUpdatingUser, handleClickShowModal}) => {
   return (
     <div>
-        <section>
+        <section className='userList'>
         {
           users.map(user => <UserCard handleClickShowModal={handleClickShowModal} setUpdatingUser={setUpdatingUser} deleteUser={deleteUser} key={user.id} user={user}/> )
         }
